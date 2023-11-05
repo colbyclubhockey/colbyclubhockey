c3f3b4af9df85b275943a7c67a1a4db157d166a8
colbyclubhockey <144280717+colbyclubhockey@users.noreply.github.com>
2023-11-04 21:57:00 -0400

    Update activate.js
    Added roster

--- activate.js
+++ activate.js
@@ -1,6 +1,5 @@
 //hamburger menu
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
 //queryselectorall returns all items that have that class
 //right now, queryselector just locks only onto about section, so menu only exits if about section is selected
 hamburger.addEventListener("click", () => {
@@ -8,7 +7,9 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active"); 
     
 })
+const navMenu = document.querySelector(".nav-menu");
 //e stands for event
+//gets rid of sidebar menu if user clicks option
 navMenu.addEventListener("click", (e)=>{
     //must use nav-link because i assume that text is most closely surrounded by class nav-link
     if(e.target.classList.contains("nav-link")){
@@ -16,6 +17,7 @@ navMenu.addEventListener("click", (e)=>{
         hamburger.classList.toggle("active");
     }
 })
+
 //banner carousel
 let bannerIndex = 0;
 showBanner();
@@ -48,4 +50,29 @@ function showImage() {
   image[imageIndex-1].style.display = "block";  
   setTimeout(showImage, 5000); // Change image every 5 seconds
 }
+//creates buttons
+document.querySelectorAll(".roster-carousel").forEach(carousel => {
+  const items = carousel.querySelectorAll(".roster-table");
+  const buttonsHtml = Array.from(items, ()=> {
+    //Use `` to be able to use ""
+    return `<span class="roster-button"></span>`;
+  });
+  carousel.insertAdjacentHTML("beforeend", `
+    <div class="roster-nav">
+      ${buttonsHtml.join("")}
+    </div>
+  `);
+  const buttons = carousel.querySelectorAll(".roster-button");
+  buttons.forEach((button, i) =>{
+    button.addEventListener("click", ()=>{
+      //unselect all tiems
+      items.forEach(item => item.classList.remove("roster-table-selected"));
+      buttons.forEach(button=>button.classList.remove("roster-button-selected"))
 
+      items[i].classList.add("roster-table-selected");
+      button.classList.add("roster-button-selected");
+    });
+  });
+  items[0].classList.add("roster-table-selected");
+  buttons[0].classList.add("roster-button-selected");
+});
